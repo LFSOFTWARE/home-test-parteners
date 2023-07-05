666c62fd45b64830a1c5bb1c3c0cdc26e6aa0cbf
Luiz <luiz.ssantos.ext@infracommerce.com.br>
2023-07-04 23:13:37 -0300

    refactor: move db credentials for env

--- back-end/config/database.js
+++ back-end/config/database.js
@@ -1,10 +1,11 @@
 const Sequelize = require("sequelize")
+require('dotenv').config();
 
-
-const Connection = new Sequelize('users','root','password',{
-    host:'localhost',
-    dialect: 'mysql',
-    timezone:"-03:00"
+const Connection = new Sequelize(process.env.DB_NAME,
+  process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
+  dialect: 'mysql',
+  timezone: "-03:00"
 
 })
 module.exports = Connection
